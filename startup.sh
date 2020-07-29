5f3993e644e5ea3ccd1a5b6afecd95ceedc2877b
upccfeng <upccfeng@gmail.coom>
2020-07-29 23:05:50 +0800

    add google test build and install

--- startup.sh
+++ startup.sh
@@ -1,3 +1,6 @@
+#
+# TODO: check the python3 and python3-venv is installed
+#
 python3 -m venv python-dev
 source python-dev/bin/activate
 pip3 install -r requirements.txt
--- tasks.py
+++ tasks.py
@@ -8,50 +8,76 @@ __dir__ = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, os.path.join(__dir__, 'pylib'))
 import taskrunner
 
+
+class BuildCodeException(Exception):
+    pass
+
+class PathErrorExcetpion(Exception):
+    pass
+
 @taskrunner.task
-def build_google_test():
+def build_googletest():
     BUILD_DIR = os.getcwd()
-    #
-    # TODO: check is donwload yet
-    #
-    git.Git(BUILD_DIR).clone("https://github.com/google/googletest.git")
 
-    GOOGLE_TEST_DIR = os.path.join(BUILD_DIR, 'googletest')
+    GOOGLETEST = 'googletest'
+    GOOGLETEST_DIR = os.path.join(BUILD_DIR, 'sources', GOOGLETEST)
+    if not os.path.isdir(GOOGLETEST_DIR):
+        git.Git(BUILD_DIR).clone('https://github.com/google/googletest.git', GOOGLETEST_DIR)
+
+    if not os.path.isdir(GOOGLETEST_DIR):
+        raise PathErrorExcetpion('Path: "{}" not found'.format(GOOGLETEST_DIR))
+        sys.exit(1)
+    else:
+        print('GoogleTest source path is: {}'.format(GOOGLETEST_DIR))
+
+    GOOGLETEST_BUILD_DIR = os.path.join(GOOGLETEST_DIR, 'build')
+    if not os.path.isdir(GOOGLETEST_BUILD_DIR):
+        os.mkdir(GOOGLETEST_BUILD_DIR)
+
+    if not os.path.isdir(GOOGLETEST_BUILD_DIR):
+        raise PathErrorExcetpion('Path: "{}" not found'.format(GOOGLETEST_BUILD_DIR))
+        sys.exit(1)
+    else:
+        print('GoogleTest build path is: {}'.format(GOOGLETEST_BUILD_DIR))
+
+    INSTALL_DIR = os.path.join(BUILD_DIR, 'install')
+    if not os.path.isdir(INSTALL_DIR):
+        os.mkdir(INSTALL_DIR)
+
+    if not os.path.isdir(INSTALL_DIR):
+        raise PathErrorExcetpion('Path: "{}" not found'.format(INSTALL_DIR))
+        sys.exit(1)
+    else:
+        print('GoogleTest install path is: {}'.format(INSTALL_DIR))
 
     #
-    # TODO: mkdir build folder in google test, and build in it
+    # TODO: support multiplatform (windows, linux, macos), now support linux
+    #       * use differen shell on different platform
+    #       * cmake will bring additional params, ex: -DCMAKE_CXX_COMPILER, -DCMAKE_CXX_COMPILER
     #
-    cmd = '\n'.join(['cd {}'.format(GOOGLE_TEST_DIR),
-                        'cmake .',
-                        'make'])
+    cmd = '\n'.join(['cd {}'.format(GOOGLETEST_BUILD_DIR),
+                        'cmake -DCMAKE_INSTALL_PREFIX={install_path} {src_dir}'.format(install_path=INSTALL_DIR, src_dir=GOOGLETEST_DIR),
+                        'make',
+                        'make install'])
     b_cmd = str.encode(cmd)
 
-    p = subprocess.Popen('bash', stdin=subprocess.PIPE,
-                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p = subprocess.Popen('bash', stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     p.stdin.write(b_cmd)
     p.stdin.close()
 
     stdout = []
-
     while True:
         ln = p.stdout.readline()
         if not ln:
             break
 
         sys.stdout.write(ln.decode('utf-8'))
-
         stdout.append(ln)
 
     p.wait()
 
-    # stderr = [ln.replace('\r\n', '') for ln in p.stderr.readlines()]
-
-    # return ExecResult(
-    #     return_code=p.returncode,
-    #     stdout=stdout,
-    #     stderr=stderr)
-
-
+    if p.returncode:
+        raise BuildCodeException('Build {} failed'.format(GOOGLETEST))
 
 @taskrunner.task
 def foo():
