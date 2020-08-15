594cacbe38f694f8bf0a8d5ddcfca8859c43f579
upccfeng <upccfeng@gmail.coom>
2020-08-15 13:25:31 +0800

    splite download googletest from build google test

--- tasks.py
+++ tasks.py
@@ -21,15 +21,28 @@ class BuildCodeException(Exception):
 class PathErrorExcetpion(Exception):
     pass
 
-@taskrunner.task
-def build_googletest_bk():
-    #
-    # TODO: install to googletest folder
-    #
-    BUILD_DIR = os.getcwd()
+#
+# TODO: maybe can bring the build path from parameters
+#
+BUILD_DIR = os.path.join(os.getcwd(), 'build')
+if not os.path.isdir(BUILD_DIR):
+    os.mkdir(BUILD_DIR)
 
-    GOOGLETEST = 'googletest'
-    GOOGLETEST_DIR = os.path.join(BUILD_DIR, 'sources', GOOGLETEST)
+GOOGLETEST_DIR = os.path.join(BUILD_DIR, 'sources', 'googletest')
+
+INSTALL_DIR = os.path.join(BUILD_DIR, 'install')
+if not os.path.isdir(INSTALL_DIR):
+    os.mkdir(INSTALL_DIR)
+
+if not os.path.isdir(INSTALL_DIR):
+    raise PathErrorExcetpion('Path: "{}" not found'.format(INSTALL_DIR))
+else:
+    logger.info('Install path is: {}'.format(INSTALL_DIR))
+
+@taskrunner.task
+def download_googletest():
+    logger.info('Start to donwload google test from GitHub')
+
     if not os.path.isdir(GOOGLETEST_DIR):
         git.Git(BUILD_DIR).clone('https://github.com/google/googletest.git', GOOGLETEST_DIR)
 
@@ -37,6 +50,11 @@ def build_googletest_bk():
         raise PathErrorExcetpion('Path: "{}" not found'.format(GOOGLETEST_DIR))
     else:
         logger.info('GoogleTest source path is: {}'.format(GOOGLETEST_DIR))
+
+
+@taskrunner.task
+def build_googletest():
+    logger.info('Start to build google test and install')
 
     GOOGLETEST_BUILD_DIR = os.path.join(GOOGLETEST_DIR, 'build')
     if not os.path.isdir(GOOGLETEST_BUILD_DIR):
@@ -47,14 +65,14 @@ def build_googletest_bk():
     else:
         logger.info('GoogleTest build path is: {}'.format(GOOGLETEST_BUILD_DIR))
 
-    INSTALL_DIR = os.path.join(BUILD_DIR, 'install')
-    if not os.path.isdir(INSTALL_DIR):
-        os.mkdir(INSTALL_DIR)
+    GOOGLETEST_INSTALL_DIR = os.path.join(INSTALL_DIR, 'googletest')
+    if not os.path.isdir(GOOGLETEST_INSTALL_DIR):
+        os.mkdir(GOOGLETEST_INSTALL_DIR)
 
-    if not os.path.isdir(INSTALL_DIR):
-        raise PathErrorExcetpion('Path: "{}" not found'.format(INSTALL_DIR))
+    if not os.path.isdir(GOOGLETEST_INSTALL_DIR):
+        raise PathErrorExcetpion('Path: "{}" not found'.format(GOOGLETEST_INSTALL_DIR))
     else:
-        logger.info('GoogleTest install path is: {}'.format(INSTALL_DIR))
+        logger.info('GoogleTest install path is: {}'.format(GOOGLETEST_INSTALL_DIR))
 
     #
     # TODO: support multiplatform (windows, linux, macos), now support linux
@@ -62,7 +80,7 @@ def build_googletest_bk():
     #       * cmake will bring additional params, ex: -DCMAKE_CXX_COMPILER, -DCMAKE_CXX_COMPILER
     #
     cmd = '\n'.join(['cd {}'.format(GOOGLETEST_BUILD_DIR),
-                        'cmake -DCMAKE_INSTALL_PREFIX={install_path} {src_dir}'.format(install_path=INSTALL_DIR, src_dir=GOOGLETEST_DIR),
+                        'cmake -DCMAKE_INSTALL_PREFIX={install_path} {src_dir}'.format(install_path=GOOGLETEST_INSTALL_DIR, src_dir=GOOGLETEST_DIR),
                         'make',
                         'make install'])
     b_cmd = str.encode(cmd)
@@ -83,21 +101,7 @@ def build_googletest_bk():
     p.wait()
 
     if p.returncode:
-        raise BuildCodeException('Build {} failed'.format(GOOGLETEST))
-
-@taskrunner.task
-def download_googletest():
-    #
-    # TODO: Download the googlet test code if folder is not exist.
-    #
-    print('download_googletest')
-
-@taskrunner.task
-def build_googletest():
-    #
-    # TODOO: build google test and install to google test folder
-    #
-    print('build_googletest')
+        raise BuildCodeException('Build googletest failed')
 
 @taskrunner.task
 def clean_googletest():
