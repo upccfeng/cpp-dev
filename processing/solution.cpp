4f34d05e09fb04634098b54f1c6f9cc8ed3be8de
upccfeng <upccfeng@gmail.coom>
2020-11-26 00:37:10 +0800

    update, but not complete

--- processing/solution.cpp
+++ processing/solution.cpp
@@ -1,8 +1,34 @@
-#include "solution.hpp"
+#include <string>
+#include <vector>
+#include <map>
 
-// function definition
+using namespace std;
 
-vector<int> Solution::partitionLabels(string S)
+class Solution
 {
-    return {};
-}
+    std::map<char, int> last_map;
+public:
+    // https://leetcode.com/problems/partition-labels/
+    vector<int> partitionLabels(string S)
+    {
+        for (int pos = 0; pos < S.size(); ++pos)
+        {
+            last_map[S[pos]] = pos;
+        }
+        int begin = 0;
+        int iter = 0;
+        int goal = last_map[S[0]];
+        vector<int> ans;
+        while (goal != S.size())
+        {
+            /*
+            * if iter == goal:
+            *     do something
+            * if last_map[S[iter]] > goal:
+            *     goal = last_map[S[iter]]
+            * iter++
+            */
+        }
+        return ans;
+    }
+};
