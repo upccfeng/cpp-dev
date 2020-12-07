40321b3a219c3251247824d28f5701f12caab1c8
upccfeng <upccfeng@gmail.com>
2020-12-07 16:57:44 +0800

    tmp update

--- processing/solution.cpp
+++ processing/solution.cpp
@@ -21,6 +21,19 @@ Problem: https://leetcode.com/problems/longest-palindromic-substring/
 
 HINT:
 
+Algorithm:
+
+```
+for start_c in s:
+    longgest_pal = start_c
+    r_boundary = start_c.pos() + 1
+    l_boundary = start_c.pos() - 1
+    while(??):
+        case: r == l:
+        case: r == r-1:
+        case: l == l+1:
+```
+
 Time Complexity:
 
 Space Complexity:
