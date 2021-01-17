84c9dc2b7cf393412c4e3a53980f87bd89c85934
upccfeng <upccfeng@gmail.com>
2021-01-17 19:56:53 +0800

    problem 8 AC, but need to speed up.

--- processing/solution.cpp
+++ processing/solution.cpp
@@ -3,25 +3,142 @@
 
 using namespace std;
 
-namespace pXX
+namespace p8
 {
     class Solution
     {
-    public:
-        int XX()
+        bool is_overflow(bool sign, int data, int e)
         {
-            return 0;
+            if (sign)
+            {
+                if (data <= (pow(2, 31) - 1 - e) / 10)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                if (-data >= (-pow(2, 31) + e) / 10)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+    public:
+        int myAtoi(std::string s)
+        {
+            bool sign = true;
+            long long int data = 0;
+            bool start = false;
+
+            for (const auto& e : s)
+            {
+                if (e == '-')
+                {
+                    if (start)
+                    {
+                        return sign ? data : -data;
+                    }
+
+                    start = true;
+                    sign = false;
+                }
+                else if (e == '+')
+                {
+                    if (start)
+                    {
+                        return sign ? data : -data;
+                    }
+
+                    start = true;
+                    sign = true;
+                }
+                else if (e == ' ')
+                {
+                    if (start)
+                    {
+                        break;
+                    }
+                }
+                else if (e > 57 || e < 48)
+                {
+                    if (start)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+
+                    sign = true;
+                }
+                else
+                {
+                    start = true;
+
+                    if (is_overflow(sign, data, e - 48))
+                    {
+                        return sign ? pow(2, 31) -1 : -pow(2, 31);
+                    }
+                    else
+                    {
+                        data = data * 10 + (e - 48);
+                    }
+                }
+            }
+
+            return sign ? data : -data;
         }
     };
-} // namespace pXX
+} // namespace p8
 
 /*
 # Problem:
-
+https://leetcode.com/problems/string-to-integer-atoi/
 
 ## HINT:
 
 ## Algorithm:
+
+ASCII:
+'-' -> 45
+'+' -> 43
+'0' -> 48
+'9' -> 57
+
+```pseudo code
+sign = 1
+data = 0
+start = false
+
+for e in s:
+    if e == 45:
+        sign = false
+    elif e == 43:
+        sign = true
+    elif e > 57 or e < 48:
+        sign = true
+        if start:
+            break
+    else:
+        start = true
+        if is_overflow(sign, data, e):
+            return 2^31 - 1 ? sign : -2^31
+        else:
+            data = data * 10 + (e-48)
+
+return data ? sign : -data
+```
+
 
 ## Time Complexity:
 
--- test_processing.cpp
+++ test_processing.cpp
@@ -8,15 +8,115 @@
 
 using namespace testing;
 
-namespace pXX
+namespace p8
 {
     using Solution = Solution;
-    TEST(test_XX, Case_1)
+
+    TEST(test_8, Case_1)
     {
         Solution s;
-        int input = 0;
-        int result = s.XX();
+        std::string input = "42";
+        int result = s.myAtoi(input);
+        int expect = 42;
+        ASSERT_EQ(result, expect);
+    }
+
+    TEST(test_8, Case_2)
+    {
+        Solution s;
+        std::string input = "   -42";
+        int result = s.myAtoi(input);
+        int expect = -42;
+        ASSERT_EQ(result, expect);
+    }
+
+    TEST(test_8, Case_3)
+    {
+        Solution s;
+        std::string input = "4193 with words";
+        int result = s.myAtoi(input);
+        int expect = 4193;
+        ASSERT_EQ(result, expect);
+    }
+
+    TEST(test_8, Case_4)
+    {
+        Solution s;
+        std::string input = "words and 987";
+        int result = s.myAtoi(input);
         int expect = 0;
         ASSERT_EQ(result, expect);
     }
-} // namespace pXX
+
+    TEST(test_8, Case_5)
+    {
+        Solution s;
+        std::string input = "-91283472332";
+        int result = s.myAtoi(input);
+        int expect = -pow(2, 31);
+        ASSERT_EQ(result, expect);
+    }
+
+    TEST(test_8, Case_Custmize_1)
+    {
+        Solution s;
+        std::string input = "91283472332";
+        int result = s.myAtoi(input);
+        int expect = pow(2, 31) - 1;
+        ASSERT_EQ(result, expect);
+    }
+
+    TEST(test_8, Case_Custmize_2)
+    {
+        Solution s;
+        std::string input = "-+--+-+-999";
+        int result = s.myAtoi(input);
+        int expect = 0;
+        ASSERT_EQ(result, expect);
+    }
+
+    TEST(test_8, Case_Custmize_3)
+    {
+        Solution s;
+        std::string input = "-+009";
+        int result = s.myAtoi(input);
+        int expect = 0;
+        ASSERT_EQ(result, expect);
+    }
+
+    TEST(test_8, Case_Custmize_4)
+    {
+        Solution s;
+        std::string input = "+-xx99";
+        int result = s.myAtoi(input);
+        int expect = 0;
+        ASSERT_EQ(result, expect);
+    }
+
+    TEST(test_8, Case_fail_1)
+    {
+        Solution s;
+        std::string input = "  +0 123";
+        int result = s.myAtoi(input);
+        int expect = 0;
+        ASSERT_EQ(result, expect);
+    }
+
+    TEST(test_8, Case_fail_2)
+    {
+        Solution s;
+        std::string input = "-2147483648";
+        int result = s.myAtoi(input);
+        int expect = -2147483648;
+        ASSERT_EQ(result, expect);
+    }
+
+    TEST(test_8, Case_fail_3)
+    {
+        Solution s;
+        std::string input = "-5-";
+        int result = s.myAtoi(input);
+        int expect = -5;
+        ASSERT_EQ(result, expect);
+    }
+} // namespace p8
