586ab8665797eeb3b8c7c3f940f9a55e31261ea8
fred_yu <fred_yu@edge-core.com>
2021-05-03 13:06:28 +0800

    fix msvc env bug

--- msvc_startup.bat
+++ msvc_startup.bat
@@ -2,9 +2,12 @@ set BUILD_DIR=%cd%
 
 set SOURCE_DIR=%~dp0
 python3 -m venv python-dev
-call %SOURCE_DIR%\python-dev\Scripts\activate.bat
-pip install -r %SOURCE_DIR%\requirements.txt
+call %BUILD_DIR%\python-dev\Scripts\activate.bat
+python3 -m pip install -r %SOURCE_DIR%\requirements.txt
 
-python %SOURCE_DIR%\tasks.py download_googletest
+cd %SOURCE_DIR%
+python3 tasks.py download_googletest
+cd %BUILD_DIR%
+
 cmake %SOURCE_DIR%
 cmake --build %BUILD_DIR%
